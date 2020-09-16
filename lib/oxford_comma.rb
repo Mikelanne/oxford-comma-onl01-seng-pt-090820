b319da5faade3e06fb6fbe241c9283d705abd030
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-16 21:06:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ require 'pry'
 def oxford_comma(array)
   if array.length == 1
     array.join
-  elsif
-    array.length == 2
+  elsif array.length == 2
     array.join(" and ")
-    binding.pry
+  elsif array.length > 3
+    array[-1].insert(0, "and, ")
   end
 end
