8201e764cbfee17f1365c306d7e38f8101bc71f0
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-16 21:07:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,9 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length > 3
+  elsif array.length == 3
+    binding.pry
+    
     array[-1].insert(0, "and, ")
   end
 end
